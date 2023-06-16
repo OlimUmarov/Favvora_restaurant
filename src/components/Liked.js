e37eb25c88ad37188296a336121ec80f8f2cd2bb
OlimUmarov <olimumarov2017@gmail.com>
2023-06-16 22:05:09 +0500

    added

---
+++ src/components/Liked.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Favourite = ({favDishes,dish,getFavourite}) => {
+    return (
+        <div>
+            <div className={favDishes.some(favdish=> favdish.title === dish.title)?"absolute w-6 h-6  right-2 top-4 cursor-pointer liked-dark":"absolute w-6 h-6  right-2 top-4 cursor-pointer liked"}
+                 onClick={() => {
+                     getFavourite(dish.title)
+                 }}>
+        </div>
+    );
+};
+
+export default Favourite;
